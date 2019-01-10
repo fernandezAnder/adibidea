62a32322dead703c230b4b5bb1617533ab8883eb
Ander <ander ander.fernandezla@elorrieta-errekamari.com>
2019-01-10 14:03:12 +0100

    Proiektuaren sorrera.

---
+++ src/autobusa/Autobusa.java
@@ -0,0 +1,13 @@
+package autobusa;
+
+public class Autobusa {
+	public static void gehiketa() {
+		int zbk=2;
+		int zbk2=5;
+		int emaitza=0;
+		
+		emaitza=zbk+zbk2;
+		System.out.println(emaitza);
+	}
+}
+
